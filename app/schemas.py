b42a68a586689376c4b1e9cd57e16b385856e23e
Beatriz Fernandes <beatrizcf2@al.insper.edu.br>
2022-05-15 19:57:27 -0300

    alterando models e schema

--- app/schemas.py
+++ app/schemas.py
@@ -1,36 +1,104 @@
-from typing import List, Optional
-from pydantic import BaseModel
+from typing import Optional, Dict, List, Set
+from pydantic import BaseModel, Field, HttpUrl # for request body interactions
 
 
-class ItemBase(BaseModel):
-    title: str
-    description: Optional[str] = None
 
+# Product ------------------------------------------------
+class ProductBase(BaseModel):
+    name: str = Field(None, title="The name of the product", max_length=100, example="iogurte")                              
+    description: Optional[str] = Field(None, title="The description of the product", max_length=300, example="iogurte desnatado 200g")   
+    brand: Optional[str] = Field(None, title="The brand of the item", max_length=80, example="nestle")   
+    price: float = Field(..., gt=0, description="The price must be greater than zero", example=2.7)
+    discount: Optional[float] = Field(..., ge=0, lt=1, description="The discount must be greater than 0 and less than 1", example=0.05)     
+    quantity: float = Field(..., gt=0, description="The quantity must be greater than zero", example=1)
+    url_img: Optional[str] = Field(..., description="The url of the product img", example="www.someproductimage.com")
 
-class ItemCreate(ItemBase):
+class ProductCreate(ProductBase):
     pass
 
 
-class Item(ItemBase):
-    id: int
-    owner_id: int
+class Product(ProductBase):
+    id_product: int
 
     class Config:
         orm_mode = True
 
 
-class UserBase(BaseModel):
-    email: str
+# Cart ---------------------------------------------------
+class CartBase(BaseModel):
+    id_user: int
 
+class CartCreate(CartBase):
+    pass
 
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
-    is_active: bool
-    items: List[Item] = []
+class Cart(CartBase):
+    id_cart: int
+    products: Set[str] = set() # lista de produtos unicos
 
     class Config:
         orm_mode = True
+
+
+# # Product ------------------------------------------------
+# class ProductBase(BaseModel):
+#     name: str
+#     description: Optional[str] = None
+#     brand: str
+#     price: float
+#     discount: float
+#     quantity: int
+#     url_img:
+
+
+# class ProductCreate(ProductBase):
+#     pass
+
+
+# class Product(ProductBase):
+#     id_product: int
+#     class Config:
+#         orm_mode = True
+
+# # Cart ---------------------------------------------------
+# class CarttBase(BaseModel):
+#     id_cart: int
+#     id_user: int
+#     carts: 
+
+# class CartCreate(ProductBase):
+#     pass
+
+
+
+# class ItemBase(BaseModel):
+#     title: str
+#     description: Optional[str] = None
+
+
+# class ItemCreate(ItemBase):
+#     pass
+
+
+# class Item(ItemBase):
+#     id: int
+#     owner_id: int
+
+#     class Config:
+#         orm_mode = True
+
+
+# class UserBase(BaseModel):
+#     email: str
+
+
+# class UserCreate(UserBase):
+#     password: str
+
+
+# class User(UserBase):
+#     id: int
+#     is_active: bool
+#     items: List[Item] = []
+
+#     class Config:
+#         orm_mode = True
--- app/shopcart.sql
+++ app/shopcart.sql
@@ -18,18 +18,18 @@ PRIMARY KEY (id_product)
 DROP TABLE IF EXISTS Cart;
 CREATE TABLE Cart(
 id_cart INT NOT NULL AUTO_INCREMENT,
+id_user INT NOT NULL,
 PRIMARY KEY (id_cart)
 );
 
 DROP TABLE IF EXISTS CartProduct;
 CREATE TABLE CartProduct(
-id_cartproduct INT NOT NULL AUTO_INCREMENT,
 id_cart INT NOT NULL,
 id_product INT NOT NULL,
 quantity INT NOT NULL,
-PRIMARY KEY (id_cartproduct),
-CONSTRAINT fk_product FOREIGN KEY (id_product)
-        REFERENCES Product (id_product),
-CONSTRAINT fk_cart FOREIGN KEY (id_cart)
-        REFERENCES Cart (id_cart)
+PRIMARY KEY (id_cart, id_product),
+FOREIGN KEY (id_product)
+        REFERENCES Product(id_product),
+FOREIGN KEY (id_cart)
+        REFERENCES Cart(id_cart)
 );
