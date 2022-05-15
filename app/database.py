7d2218cd6b9dc59441f6c96c59c6fdfd3d1281bc
Beatriz Fernandes <beatrizcf2@al.insper.edu.br>
2022-05-15 17:27:18 -0300

    tutorial fastApi

---
+++ app/database.py
@@ -0,0 +1,19 @@
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Cria o URL do database para o SQLAlchemy
+DATABASE_URL = "mysql+mysqlconnector://megadados:megadados123@localhost:3306/teste_orm"
+
+# Cria o engine do SQLAlchemy
+engine = create_engine(DATABASE_URL)
+
+# Cria uma classenSessionLocal
+# Cada instancia de Session é uma sessão do database
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Cria a classe Base
+# Usada para criar os modelos ou classe do database (modelos ORM)
+Base = declarative_base()
+
